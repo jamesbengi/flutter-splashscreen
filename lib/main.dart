68907b25c8f2b6127fe8ac3c93067812b498c09d
james bengi <jamesbengi21@gmail.com>
2021-11-15 14:37:59 +0300

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -5,163 +5,126 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+ 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'travel Kenya',
       theme: ThemeData(
-       
-        primarySwatch: Colors.grey,
+       primaryColor: Colors.amberAccent
       ),
-      home: MyHomePage(title: 'DREAM TOURS'),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
       
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
+class _SplashScreenState extends State<SplashScreen> {
+ void initState(){
+   super.initState();
+   Navigator.of(context).pushReplacement(
+     MaterialPageRoute(builder: (context)=>HomeScreen()
+     )
+   );
+ }
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
-      appBar: AppBar(
-       
-        title: Text(widget.title),
-        backgroundColor: Colors.green[700],
-      ),
-      backgroundColor: Colors.green[100],
-      drawer: new Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName:new Text("JAMES BENGI",style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),
-              accountEmail: new Text("jamesbengi21@gmail.com",style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
-              currentAccountPicture: CircleAvatar(
-                radius: 90,
-                backgroundColor: Colors.black,
-              ),
-               ),
-            ListTile(
-              leading: Icon(Icons.person,size: 40,),
-              title: Text("profile",style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+      backgroundColor:Colors.green[800],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget> [
+            
+            
+
+             
+             SizedBox(height: 100,),
+            Text("What's App",style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
+            SizedBox(height: 30,),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              
             ),
-            ListTile(
-              leading: Icon(Icons.info,size: 40,),
-              title: Text("About us",style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 16)),
-            ),ListTile(
-              leading: Icon(Icons.password,size: 40,),
-              title: Text("password",style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 16)),
-            ),
+            SizedBox(height: 400,),
+            Text("from facebook",style: TextStyle(color: Colors.white),)
+            
           ],
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(30.0),
-        child: GridView.count(crossAxisCount:2,
-        children:<Widget> [
-          Card(
-            child: InkWell(
-              onTap: (){},
-              splashColor: Colors.green,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget> [
-                    Icon(Icons.home,size: 70.0,),
-                    Text("image")
-                  ],
-                ),
-              ),
-            ),
+      
+    );
+  }
+}
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+     appBar: AppBar(
+       backgroundColor: Colors.green[900],
+       title: Text("whatsApp",style: TextStyle(color: Colors.white),),
+     ),
+      body: ListView.builder(itemBuilder: (BuildContext context,int index){
+        
+        return Card(
+          color: Colors.white,
+          child: ListTile(
+            title: Text(names[index]),
           ),
-           Card(
-            child: InkWell(
-              onTap: (){},
-              splashColor: Colors.green,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget> [
-                    Icon(Icons.mail,size: 70.0,),
-                    Text("email")
-                  ],
-                ),
-              ),
-            ),
-          ), Card(
-            child: InkWell(
-              onTap: (){},
-              splashColor: Colors.green,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget> [
-                    Icon(Icons.alt_route,size: 70.0,),
-                    Text("route")
-                  ],
-                ),
-              ),
-            ),
-          ), Card(
-            child: InkWell(
-              onTap: (){},
-              splashColor: Colors.green,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget> [
-                    Icon(Icons.call,size: 70.0,),
-                    Text("call")
-                  ],
-                ),
-              ),
-            ),
-          ), Card(
-            child: InkWell(
-              onTap: (){},
-              splashColor: Colors.green,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget> [
-                    Icon(Icons.share_location,size: 70.0,),
-                    Text("share location")
-                  ],
-                ),
-              ),
-            ),
-          ), Card(
-            child: InkWell(
-              onTap: (){},
-              splashColor: Colors.green,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget> [
-                    Icon(Icons.facebook,size: 70.0,),
-                    Text("facebook")
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-        ),
+        );
+
+      },
+      itemCount: 20,
       ),
-    
+    );
+  }
+}
+class NamesList extends StatelessWidget {
+  
+   NamesList({ Key? key }) : super(key: key);
+ final List names=[
+              "james",
+              " jesse",
+              " jack",
+              " marcus",
+              " anita",
+              " jose",
+              " crush",
+              " bro",
+              " kamata",
+              " justo",
+              " kama",
+              " nito",
+              " orio",
+              " njugan",
+              " mzae",
+              " mokoro",
+              " zakayo",
+              " peter",
+              " nito",
+              " msee",
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
